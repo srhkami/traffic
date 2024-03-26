2bbde5af3ee079f8f57e4e8e0ab81a9910bb0c02
srhkami <160311248+srhkami@users.noreply.github.com>
2024-03-26 13:07:34 +0800

    0.12

--- script/regulations.js
+++ script/regulations.js
@@ -1,5 +1,9 @@
 import {list_PH} from './regulations/PH.js'
+import {list_SR} from './regulations/SR.js'
+import {list_DR} from './regulations/DR.js'
 
+
+// 函式：取得條文內容
 function get_text(article){
     let text = '';
     act_list.forEach((value)=>{
@@ -10,13 +14,32 @@ function get_text(article){
     return text;
 }
 
-let act_list = []
-let act_name = new URLSearchParams(location.search);
-act_name = act_name.get('name');
-if (act_name == 'PH'){
-    act_list = list_PH;
+//函式：判斷是哪種法規
+function get_regulation(act_name){
+    let act_list = []
+    if (act_name == 'PH'){
+        act_list = list_PH;
+        document.querySelector('title').innerHTML = '處罰條例';
+        document.querySelector('.page_title').innerHTML = '處罰條例';
+    }
+    else if (act_name == 'SR'){
+        act_list = list_SR;
+        document.querySelector('title').innerHTML = '安全規則';
+        document.querySelector('.page_title').innerHTML = '安全規則';
+    }
+    else if (act_name == 'DR'){
+        act_list = list_DR;
+        document.querySelector('title').innerHTML = '處理細則';
+        document.querySelector('.page_title').innerHTML = '處理細則';
+    }
+    return act_list;
 }
 
+//主程式開始
+let act_name = new URLSearchParams(location.search);
+act_name = act_name.get('name');
+const act_list = get_regulation(act_name);
+// 刷新條目清單
 let html = '';
 act_list.forEach((value) => {
     html = html + `
@@ -24,10 +47,10 @@ act_list.forEach((value) => {
         <div class="div_article">第 ${value.article} 條</div>
         <div class="div_title">${value.title}</div>
     </li>
-    `
+    `;
 });
 document.querySelector('.list_article').innerHTML = html;
-
+//偵測每一條的點擊
 document.querySelectorAll('.block_list').forEach((button)=>{
     const article = button.dataset.article;
     button.addEventListener('click',(click)=>{
