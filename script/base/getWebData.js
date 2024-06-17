06a424c2a54419d4690ffbc6d05576b149de136f
srhkami <160311248+srhkami@users.noreply.github.com>
2024-06-17 23:33:45 +0800

    Delete script/base/getWebData.js

--- script/base/getWebData.js
+++
@@ -1,5 +0,0 @@
-export class WebData {
-  constructor(sheetID, sheetName, apiKey) {
-    this.url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${sheetName}?alt=json&key=${apiKey}`;
-  }
-}
