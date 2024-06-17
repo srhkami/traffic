c2ca99994180a50901c681565c114f67c5062538
srhkami <160311248+srhkami@users.noreply.github.com>
2024-06-17 23:39:36 +0800

    1.20.3

--- script/base/pages.js
+++ script/base/pages.js
@@ -1,4 +1,4 @@
-import { list_PH, list_SR, list_DR, list_ML, list_PA, list_PW, list_TA, list_CC, list_CP, list_SO, list_VS } from '../data/rg_list.js';
+import { list_PH, list_SR, list_DR, list_ML, list_PA, list_PW, list_TA, list_CC, list_CP, list_SO, list_VS } from '../list/rg_list.js';
 
 //頁面預設類別
 class Page {
